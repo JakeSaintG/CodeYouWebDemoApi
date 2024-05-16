7e82b9338b08d5287338052dbe8584c3687ba710
JakeSaintG <jakesaint27@gmail.com>
2024-05-15 23:17:37 -0400

    Continued work on dbutils

--- utils/dbutils.ts
+++ utils/dbutils.ts
@@ -1,17 +1,24 @@
 import sqlite from 'sqlite3';
+import { v4 as uuidv4 } from 'uuid';
 
 export class DbUtils {
     
     public dbContext!: sqlite.Database;
-    private createTable: string = `
-        CREATE TABLE employees (
-            employee_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-            last_name NVARCHAR(20)  NOT NULL,
-            first_name NVARCHAR(20)  NOT NULL,
-            title NVARCHAR(20),
-            address NVARCHAR(100),
-            country_code INTEGER
-        )
+
+    /*
+        Create table:
+            For the contact_id, it is totally valid to use an auto-incrementing ID instead of manually assigning one
+            Notice that the rest of the columns do not specify a character length.
+                In SQLite, the max size of a VARCHAR column is not enforced and not specifying is valid in a create statement.
+    */
+    private createContactsTable: string = `
+        CREATE TABLE contacts (
+            contact_id VARCHAR(36) PRIMARY KEY NOT NULL,
+            name VARCHAR NOT NULL,
+            email VARCHAR NOT NULL,
+            pieces_of_interest VARCHAR NOT NULL,
+            message VARCHAR NOT NULL
+        ) 
     `;
 
     constructor() {
@@ -19,19 +26,20 @@ export class DbUtils {
     }
 
     public createDbContext = () => {
-        this.dbContext = new sqlite.Database('./emp_database.db', (error: Error | null) => {
+        this.dbContext = new sqlite.Database('./contacts.db', (error: Error | null) => {
             if (error) {
                 console.error(`Error opening database: ${error.message}`);
             } else {
         
-                this.dbContext.run(this.createTable, (err: unknown) => {
-                    if (err) {
-                        console.log("Table already exists.");
+                this.dbContext.run(this.createContactsTable, (err: Error | null) => {
+
+                    if (err && !err.message.includes("already exists")) {
+                        console.log(err.message);
                     }
-                    let insert = 'INSERT INTO employees (last_name, first_name, title, address, country_code) VALUES (?,?,?,?,?)';
-                    this.dbContext.run(insert, ["Chandan", "Praveen", "SE", "Address 1", 1]);
-                    this.dbContext.run(insert, ["Samanta", "Mohim", "SSE", "Address 2", 1]);
-                    this.dbContext.run(insert, ["Gupta", "Pinky", "TL", "Address 3", 1]);
+                    let insert = 'INSERT INTO contacts (contact_id, name, email) VALUES (?,?,?)';
+                    this.dbContext.run(insert, [uuidv4(), "name", "email", "['piece one,'piece two']", "message"]);
+                    this.dbContext.run(insert, [uuidv4(), "name", "email", "['piece one,'piece two']", "message"]); 
+                    this.dbContext.run(insert, [uuidv4(), "name", "email", "['piece one,'piece two']", "message"]); 
                 });
             }
         });
