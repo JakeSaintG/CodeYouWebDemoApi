187014e594fc41579afce9c261333a590c3a2e69
JakeSaintG <jakesaint27@gmail.com>
2024-05-22 22:28:02 -0400

    update unit tests, readme, and template data

--- readme.md
+++ readme.md
@@ -1,5 +1,26 @@
 # Contacts API
+This ContactRequest API have been developed for the purpose of giving Code:YOU students a local API to practice working with. 
+
+## Synopsis
+You have been tasked with developing a web page for an artist that would like intested parties to contact them about various art pieces. They had a backend developer set up this ContactRequest API for them and have supplied this GitHub repository for you to work with. You have complete creative freedom for this contact request form! Best of luck!
+
 ## Installation
+Before cloning down this repo, first verify that recent versions of Node and npm are installed on you machine.
+- Check for installation by running the below commands:
+> npm -v
+> node -v
+
+- If version number are NOT returned, install Node via your preferred method. Here is a place to get started:
+https://nodejs.org/en/download/prebuilt-installer
+
+- If version numbers are returned, go ahead and open a terminal to clone down this repo.
+
+After cloning the repo, use your terminal to navigate (`cd`) to the root project directory that contains the package.json. Use the following npm command to install the API's dependancies:
+> npm install 
+
+- build
+- start
+- safely closing
 
 ## Use
 ### Port
@@ -11,16 +32,18 @@ Work in Progress
 ### Resetting Data
 
 ## Troubleshooting:
-mac command to find process that needs to be terminated:
-`sudo lsof -i:8000`
+### Mac
+If an error comes up for the port already being in use on a Mac:
+- Open a terminal and enter the below command to check the process id (PID) assigned to port 8000
+  - > sudo lsof -i:8000
+  - Note: if the port was altered in the .env file, replace 8000 above with the assigned port.
+- Take note of the process ID (PID). This is what you will look for in Activity Monitor for the process to terminate.
 
-windows command to find the process that needs to be terminated:
+### Windows
+If an error comes up for the port already being in use on a Windows PC:
 ``
 
 ## Inquiring Minds
-While this project is intended for practice making API calls, the hope is that inquiring minds go digging to see what is going on under the hood. In this Express API are many concepts that will likely not yet be covered yet at this point including TypeScript, Node, Express, Object Oriented Programming, SQLite, and a few others. Ideally, when these concepts are covered in class, this project can also serve as a reference so you can see these topics in action!
+While this project is intended for practice making API calls, the hope is that inquiring minds go digging to see what is going on under the hood. There are many concepts in this Express API that have likely not yet be covered yet at this point including TypeScript, Node, Express, Object Oriented Programming, SQLite, and a few others. Ideally, when these concepts are covered in class, this project can also serve as a reference so you can see these topics in action!
 
 NOTE! Most developers would consider this project to be "overly commented". This has been done due to the educational nature of the project. Usually, there is not a need to comment on what every line of code. Consider this a "do as I say, not as I do" moment. 
-
-
-
--- tests/contactRepository.spec.ts
+++ tests/contactRepository.spec.ts
@@ -1,57 +1,63 @@
-// import { ContactRepository } from '../src/repositories/contactRepository';
-// import fs from 'fs';
+import { ContactRepository } from '../src/repositories/contactRepository';
+import fs from 'fs';
+import { DbUtils } from '../utils/dbutils';
+import { IResponse } from '../src/interfaces/IResponse';
+import { IContactRequest } from '../src/interfaces/IContactRequest';
 
-// /*
-//     The following lines is one of many ways to overwrite functions.
-//     In this case, the goal is to mock/overwrite the fs module used in contactRepository.
-// */
-// jest.mock('fs');
-// const mockFS: jest.Mocked<typeof fs> = <jest.Mocked<typeof fs>>fs;
+/*
+    The following lines is one of many ways to overwrite functions.
+    In this case, the goal is to mock/overwrite the fs module used in contactRepository.
+*/
+jest.mock('fs');
+const mockFS: jest.Mocked<typeof fs> = <jest.Mocked<typeof fs>>fs;
+jest.mock('../utils/dbutils')
+const mockDbUtils: jest.Mocked<typeof DbUtils> = <jest.Mocked<typeof DbUtils>>DbUtils;
+/*
+    The AAA patten is commonly used in unit tests.
+        Arrange - Perform step neccessary to carry out the test. (Mock functions, set variables, etc)
+        Act - Call the method or functionality that is under test. Commonly used variables are SUT or SystemUnderTest.
+        Assert - Check the results of the SystemUnderTest (referred to as "actual") against the expected results.
+*/
+it('should return an error if template file not present when resetting data', () => {
+    //Arrange
+    const dbUtils = new DbUtils(); // Reminder that dbUtils is mocked on line 12 so this instance doesn't do anything.
+    const contactRepository = new ContactRepository(dbUtils);
+    mockFS.existsSync.mockReturnValue(false);
+    const expectedCode = 500;
+    const expectedMessage = 'Proper files not in place. POST to the reset endpoint, restart server, or replace files to proceed.';
 
-// /*
-//     The AAA patten is commonly used in unit tests.
-//         Arrange - Perform step neccessary to carry out the test. (Mock functions, set variables, etc)
-//         Act - Call the method or functionality that is under test. Commonly used variables are SUT or SystemUnderTest.
-//         Assert - Check the results of the SystemUnderTest (referred to as "actual") against the expected results.
-// */
-// it('should return an error if template file not present when resetting data', async () => {
-//     //Arrange
-//     const contactRepository = new ContactRepository();
-//     mockFS.existsSync.mockReturnValue(false);
-//     const expectedCode = 500;
-//     const expectedMessage = 'Template JSON file not found. Unable to set or reset contact data.';
+    //Act
+    const SystemUnderTest = contactRepository.ensureDataFilesExist() as IResponse;
 
-//     //Act
-//     const SystemUnderTest = await contactRepository.resetContactData();
+    //Assert
+    const actualCode = SystemUnderTest.code;
+    const actualMessage = SystemUnderTest.message;
+    expect(actualCode).toEqual(expectedCode);
+    expect(actualMessage).toEqual(expectedMessage);
+});
 
-//     //Assert
-//     const actualCode = SystemUnderTest.code;
-//     const actualMessage = SystemUnderTest.message;
-//     expect(actualCode).toEqual(expectedCode);
-//     expect(actualMessage).toEqual(expectedMessage);
-// });
+/*
+    This one is a little more complicated.
+    The test above covered the "fail" scenario for resetContactData(). The "success" case should now be tested.
+    Like the test above, it is necessary to mock some of what the fs module is doing: existsSync and readFileSync. 
+    After that, "spies" are created to watch the private methods resetDbFromTemplateData() and resetJsonFileFromTemplate().
+        It is also possible to overwrite their functionality so they don't actually try to reset anything.
+            ".mockImplementation( () => {} )" bascially makes them do nothing.
+    The resetContactData() method is then called with the expectation that resetDbFromTemplateData() and resetJsonFileFromTemplate() are as well.
+*/
+it('should call the private methods involved in resetting files when main reset method is called', async () => {
+    //Arrange
+    const dbUtils = new DbUtils();
+    const contactRepository = new ContactRepository(dbUtils);
+    mockFS.existsSync.mockReturnValue(true);
+    mockFS.readFileSync.mockReturnValue('[{"foo":"bar"}]');
+    const resetDbSpy = jest.spyOn( contactRepository as any, 'resetDbFromTemplate').mockImplementation( () => {} );
+    const resetJsonFileSpy = jest.spyOn( contactRepository as any, 'resetJsonFromTemplate').mockImplementation( () => {} );
 
-// /*
-//     This one is a little more complicated.
-//     The test above covered the "fail" scenario for resetContactData(). The "success" case should now be tested.
-//     Like the test above, it is necessary to mock some of what the fs module is doing: existsSync and readFileSync. 
-//     After that, "spies" are created to watch the private methods resetDbFromTemplateData() and resetJsonFileFromTemplate().
-//         It is also possible to overwrite their functionality so they don't actually try to reset anything.
-//             ".mockImplementation( () => {} )" bascially makes them do nothing.
-//     The resetContactData() method is then called with the expectation that resetDbFromTemplateData() and resetJsonFileFromTemplate() are as well.
-// */
-// it('should call the private methods involved in resetting files when main reset method is called', async () => {
-//     //Arrange
-//     const contactRepository = new ContactRepository();
-//     mockFS.existsSync.mockReturnValue(true);
-//     mockFS.readFileSync.mockReturnValue('[{"foo":"bar"}]');
-//     const resetDbSpy = jest.spyOn( contactRepository as any, 'resetDbFromTemplateData').mockImplementation( () => {} );
-//     const resetJsonFileSpy = jest.spyOn( contactRepository as any, 'resetJsonFileFromTemplate').mockImplementation( () => {} );
+    //Act
+    contactRepository.resetContactData();
 
-//     //Act
-//     contactRepository.resetContactData();
-
-//     //Assert
-//     expect(resetJsonFileSpy).toHaveBeenCalled();
-//     expect(resetDbSpy).toHaveBeenCalled();
-// });
+    //Assert
+    expect(resetJsonFileSpy).toHaveBeenCalled();
+    expect(resetDbSpy).toHaveBeenCalled();
+});
