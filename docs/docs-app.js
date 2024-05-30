30c10a9ec00b4e356f0761155522dc919e78fb93
Jake St. Germain <jakesaint27@gmail.com>
2024-05-30 00:35:32 -0400

    create basic data viewer modal

--- docs/docs-app.js
+++ docs/docs-app.js
@@ -1,7 +1,8 @@
 let port = '8000';
 let pingInterval = 0;
 let serverPreviouslyHealthy = false;
-const serverStatus = document.getElementById('server-status');
+const serverStatus = document.getElementById('server_status');
+const dialog = document.querySelector('dialog');
 
 const checkPing = () => {
     const check = setInterval(() => {
@@ -33,21 +34,72 @@ const showServerHealth = (serverOnline) => {
         serverPreviouslyHealthy = true;
 
         // Removing an element class that may not exist is "safe". No need to check element.classList before removing.
-        serverStatus.parentElement.classList.remove('server-unhealthy');
-        serverStatus.parentElement.classList.add('server-healthy');
+        serverStatus.parentElement.classList.remove('server_unhealthy');
+        serverStatus.parentElement.classList.add('server_healthy');
     } else if (!serverOnline) {
         serverPreviouslyHealthy = false;
-        serverStatus.parentElement.classList.remove('server-healthy');
-        serverStatus.parentElement.classList.add('server-unhealthy');
+        serverStatus.parentElement.classList.remove('server_healthy');
+        serverStatus.parentElement.classList.add('server_unhealthy');
     }
 };
 
 const clearData = () => {
-    console.log('clearing data...')
-}
+    console.log('clearing data...');
+};
 
 const resetData = () => {
-    console.log('resetting data...')
+    console.log('resetting data...');
+};
+
+const showData = (show) => {
+    if (show) {
+        dialog.showModal();
+        getData();
+        return;
+    }
+    dialog.close();
+};
+
+const getData = () => {
+    fetch(`http://localhost:${port}/contact`)
+        .then(async (r) => await r.json())
+        .then((json) => {
+            const htmlTable = generateHtmlTable(json.data);
+            document.getElementById('contact_request_data').appendChild(htmlTable);
+        });
+};
+
+
+const generateHtmlTable = (jsonData) => {
+    const table = document.createElement('table');
+    const headerRow = document.createElement('tr');
+
+    Object.keys(jsonData[0]).forEach((key) => {
+        const columnHead = document.createElement('th');
+        columnHead.textContent = camelCaseToWords(key);
+        headerRow.appendChild(columnHead);
+    });
+
+    table.appendChild(headerRow);
+
+    jsonData.forEach((rowData) => {
+        const tableRow = document.createElement('tr');
+
+        Object.values(rowData).forEach((value) => {
+            const tableCell = document.createElement('td');
+            tableCell.textContent = value;
+            tableRow.appendChild(tableCell);
+        });
+
+        table.appendChild(tableRow);
+    });
+
+    return table;
 }
+
+const camelCaseToWords = (camelCase) => {
+    const wordsSplit = camelCase.replace(/([A-Z])/g, ' $1');
+    return wordsSplit.charAt(0).toUpperCase() + wordsSplit.slice(1);
+};
 
 checkPing();
