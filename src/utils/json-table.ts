346338d8c57ea616006f91210a24de8595537a6d
JakeSaintG <jakesaint27@gmail.com>
2024-06-11 21:46:16 -0700

    Refactor JSON table

--- src/utils/json-table.ts
+++ src/utils/json-table.ts
@@ -15,13 +15,13 @@ export class JsonTable<E> {
         this.defaultDataFileLocation = defaultDataFileLocation;
     }
 
-    init() {
+    init = () => {
         if (!fs.existsSync(this.fileLocation)) {
             this.write(this.readDefaults());
         }
     }
 
-    readDefaults(): E[] {
+    readDefaults = (): E[] => {
         if (!fs.existsSync(this.defaultDataFileLocation)) {
             throw new Error('Template JSON file not found - please replace this file and restart the server. Ask a mentor for the file if you do not have a copy.');
         }
@@ -29,7 +29,7 @@ export class JsonTable<E> {
         return JSON.parse(fs.readFileSync(this.defaultDataFileLocation, 'utf8'));
     }
 
-    read(): E[] {
+    read = (): E[] => {
         if (!fs.existsSync(this.fileLocation)) {
             throw new Error(`Data JSON file '${this.fileLocation}' not in place. POST to the reset endpoint, restart server, or replace files to proceed.`)
         }
@@ -37,17 +37,17 @@ export class JsonTable<E> {
         return JSON.parse(fs.readFileSync(this.fileLocation, 'utf8'));
     }
 
-    write(json: E[]) {
+    write = (json: E[]) => {
         fs.writeFileSync(this.fileLocation, JSON.stringify(json, null, 2));
     }
 
-    insert(entry: E) {
+    insert = (entry: E) => {
         let contactData = this.read();
         contactData.push(entry);
         this.write(contactData);
     }
 
-    clear() {
+    clear = () => {
         this.write([]);
     }
 }
